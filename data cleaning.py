d66a7233861edc45474d897aebb4fb4ba69664cf
boldogable <123760066+boldogable@users.noreply.github.com>
2023-06-06 16:10:25 +0200

    Update data cleaning.py
    The code is organized into functions to promote reusability and modularity.
    Descriptive function names are used to convey the purpose of each function.
    The file path, column mapping, column order, column name, and outlier threshold are parameterized for flexibility.
    The main logic is encapsulated within the main() function.
    Error handling and validation can be added as needed to handle potential exceptions.

--- data cleaning.py
+++ data cleaning.py
@@ -1,25 +1,52 @@
 import pandas as pd
 
-# CSV fájl beolvasása
-data = pd.read_csv('adatok.csv')
+def load_data(file_path):
+# Read CSV file
+data = pd.read_csv(file_path)
+return data
 
-# Hiányzó értékek kezelése
-data = data.fillna(0)  # Hiányzó értékek feltöltése nullával
+def handle_missing_values(data):
+# Fill missing values with zero
+data = data.fillna(0)
+return data
 
-# Oszlopok átnevezése
-data = data.rename(columns={'regi_oszlop_nev': 'uj_oszlop_nev'})
+def rename_columns(data, column_mapping):
+# Rename columns based on the provided mapping
+data = data.rename(columns=column_mapping)
+return data
 
-# Oszlopok átrendezése
-data = data[['oszlop1', 'oszlop2', 'oszlop3']]
+def reorder_columns(data, column_order):
+# Reorder columns based on the provided order
+data = data[column_order]
+return data
 
-# Outlier-ek kezelése
-def outlier_kezeles(sor):
-    if sor['oszlop3'] > 1000:
-        return 1000
-    else:
-        return sor['oszlop3']
+def handle_outliers(data, column_name, threshold):
+# Handle outliers in the specified column
+data.loc[data[column_name] > threshold, column_name] = threshold
+return data
 
-data['oszlop3'] = data.apply(outlier_kezeles, axis=1)
+def main():
+# Load the data
+file_path = 'data.csv'
+data = load_data(file_path)
 
-# Tisztított adatkészlet kiíratása
+# Handle missing values
+data = handle_missing_values(data)
+
+# Rename columns
+column_mapping = {'old_column_name': 'new_column_name'}
+data = rename_columns(data, column_mapping)
+
+# Reorder columns
+column_order = ['column1', 'column2', 'column3']
+data = reorder_columns(data, column_order)
+
+# Handle outliers
+column_name = 'column3'
+threshold = 1000
+data = handle_outliers(data, column_name, threshold)
+
+# Print the cleaned dataset
 print(data.head())
+if name == 'main':
+main()
