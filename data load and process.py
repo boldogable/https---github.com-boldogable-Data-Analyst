0bc24986ada5018ddd43cb8d3f9310abf56452a6
boldogable <123760066+boldogable@users.noreply.github.com>
2023-06-06 16:14:59 +0200

    Update data load and process.py
    The code is further modularized into functions for each specific task.
    The matplotlib.pyplot library is imported to handle data visualization.
    Functions are added to encapsulate each step of the process, making the code more readable and maintainable.
    The visualize_data function now shows the plot using plt.show() for better visualization.
    The main logic is encapsulated within the main() function.

--- data load and process.py
+++ data load and process.py
@@ -1,18 +1,48 @@
 import pandas as pd
+import matplotlib.pyplot as plt
 
-# CSV fájl beolvasása
-data = pd.read_csv('adatok.csv')
+def load_data(file_path):
+# Read CSV file
+data = pd.read_csv(file_path)
+return data
 
-# Adatok áttekintése
+def display_overview(data):
+# Overview of the data
 print(data.head())
 
-# Statisztikai információk kinyerése
+def extract_summary(data):
+# Extract statistical information
 summary = data.describe()
+return summary
+
+def extract_column(data, column_name):
+# Extract a specific column
+column = data[column_name]
+return column
+
+def visualize_data(data, column_name):
+# Data visualization
+data[column_name].plot(kind='hist')
+plt.show()
+
+def main():
+
+# Load the data
+file_path = 'data.csv'
+data = load_data(file_path)
+# Display overview
+display_overview(data)
+
+# Extract summary
+summary = extract_summary(data)
 print(summary)
 
-# Adott oszlop kiemelése
-column = data['oszlop_neve']
+# Extract a specific column
+column_name = 'column_name'
+column = extract_column(data, column_name)
 print(column)
 
-# Adatok vizualizációja
-data['oszlop_neve'].plot(kind='hist')
+# Visualize data
+visualize_data(data, column_name)
+if name == 'main':
+main()
