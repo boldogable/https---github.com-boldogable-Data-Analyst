26fffa38cb0d114b2f490e93bb911ba7362c506a
boldogable <123760066+boldogable@users.noreply.github.com>
2023-06-06 16:16:19 +0200

    Update data visualization.py
    The code is modularized into functions for better organization and reusability.
    The load_data() function loads the data from the CSV file.
    The visualize_data() function handles the data visualization using a bar plot and customizable labels.
    The main logic is encapsulated within the main() function.
    The function parameters column_name and value_name allow flexibility to choose the specific columns to visualize.
    The code is written in English, including comments and variable names, to enhance readability.
    The code can be easily expanded by adding additional functions or error handling, as needed.

--- data visualization.py
+++ data visualization.py
@@ -1,14 +1,32 @@
 import pandas as pd
 import matplotlib.pyplot as plt
 
-# Adatok betöltése
-data = pd.read_csv('adatok.csv')
+def load_data(file_path):
+# Load data from CSV file
+data = pd.read_csv(file_path)
+return data
 
-# Adatok vizualizációja
-plt.bar(data['oszlop_nev'], data['ertek'])
-plt.xlabel('Oszlop neve')
-plt.ylabel('Érték')
-plt.title('Adatok vizualizációja')
+def visualize_data(data, column_name, value_name):
+# Data visualization
+plt.bar(data[column_name], data[value_name])
+plt.xlabel(column_name)
+plt.ylabel(value_name)
+plt.title('Data Visualization')
 
-# Diagram megjelenítése
+bash
+Copy code
+# Show the plot
 plt.show()
+def main():
+# Load the data
+file_path = 'data.csv'
+data = load_data(file_path)
+
+kotlin
+Copy code
+# Visualize data
+column_name = 'column_name'
+value_name = 'value'
+visualize_data(data, column_name, value_name)
+if name == 'main':
+main()
